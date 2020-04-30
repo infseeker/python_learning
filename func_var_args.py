d5a977103b2ae097854543635f89af999765409f
infseek <infseek@gmail.com>
2020-04-30 18:36:36 +0300

    var args (*args, **kwargs), only **kwargs

--- func_var_args.py
+++ func_var_args.py
@@ -10,4 +10,19 @@ def total(a=5, *numbers, **phonebook):
         print(first_part, second_part)
 
 
-print(total(10, 1, 2, 3, Jack=1123, John=2231, Inge=1560))
+total(10, 1, 2, 3, Jack=1123, John=2231, Inge=1560)
+
+
+# Keywords only
+def total_1(initial=5, *numbers, extra_number=10):
+    count = initial
+    for number in numbers:
+        count += number
+    count += extra_number
+    print(count)
+
+
+total_1(10, 1, 2, 3, extra_number=50)
+total_1(10, 1, 2, 3)
+# Вызовет ошибку, поскольку мы не указали значение
+# аргумента по умолчанию для 'extra_number'.
