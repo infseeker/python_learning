11142e5965989cbf3e0c53d8f08778cc7e2db5a0
infseek <infseek@gmail.com>
2020-04-30 18:39:39 +0300

    few changes

--- func_global_nonlocal.py
+++ func_global_nonlocal.py
@@ -10,3 +10,18 @@ def func():
 
 func()
 print('Значение x составляет', x)
+
+
+def func_outer(a):
+    x = a
+    print('x равно', x)
+
+    def func_inner():
+        nonlocal x
+        x *= 2
+
+    func_inner()
+    print('Локальное x сменилось на', x)
+
+
+func_outer(5)
