0f4eaeae06ef8fe96d08fcbdade2e6c9e662bb89
infseek <infseek@gmail.com>
2020-05-01 19:21:55 +0300

    dict, sequences

---
+++ data_structures/sequences.py
@@ -0,0 +1,13 @@
+shop_list = ['apples', 'mangoes', 'carrots', 'bananas']
+name = 'Some String'
+
+print(
+    shop_list[0],
+    shop_list[3],
+    shop_list[-1],
+    name[0],
+    shop_list[:-1],
+    name[:-1],
+    name[-2],
+    sep='\n'
+)
